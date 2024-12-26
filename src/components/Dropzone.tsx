b413b61410d4951e884028bec9c1312a7bd146de
m19e <m19e.dev@gmail.com>
2024-12-26 16:31:10 +0900

    Replace by jotai state

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,27 +1,33 @@
 'use client'
-import { useCallback, useState } from 'react'
+import { useAtom } from 'jotai'
+import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 
+import { baseImgUrlAtom } from '@/store/atoms'
+
 export const Dropzone = () => {
-  const [url, setUrl] = useState<string | undefined>(undefined)
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    if (acceptedFiles.length === 0) {
-      return
-    }
-    const files = acceptedFiles.map((file) => ({
-      file,
-      url: URL.createObjectURL(file),
-    }))
-    setUrl(files[0].url)
-  }, [])
+  const [url, setUrl] = useAtom(baseImgUrlAtom)
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      if (acceptedFiles.length === 0) {
+        return
+      }
+      const files = acceptedFiles.map((file) => ({
+        file,
+        url: URL.createObjectURL(file),
+      }))
+      setUrl(files[0].url)
+    },
+    [setUrl],
+  )
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
+    maxFiles: 1,
     accept: {
       'image/png': ['.png'],
       'image/jpeg': ['.jpeg', '.jpg'],
       'image/webp': ['.webp'],
     },
-    maxFiles: 1,
   })
 
   return (
