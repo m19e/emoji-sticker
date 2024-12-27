8b755cef4b083f097149da6f95cd346aad747462
m19e <m19e.dev@gmail.com>
2024-12-27 16:29:06 +0900

    Add effect for revoke expired base-img url

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,3 +1,13 @@
+import { atomEffect } from 'jotai-effect'
+import { withHistory } from 'jotai-history'
 import { atomWithReset } from 'jotai/utils'
 
 export const baseImgUrlAtom = atomWithReset<string | undefined>(undefined)
+
+const baseImgUrlHistory = withHistory(baseImgUrlAtom, 2)
+export const revokeEffect = atomEffect((get) => {
+  const [url, prevUrl] = get(baseImgUrlHistory)
+  if (typeof url === 'undefined') {
+    prevUrl && URL.revokeObjectURL(prevUrl)
+  }
+})
