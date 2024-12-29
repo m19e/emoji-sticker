7bb1b0014535e882b00ec9d9832300771feabab2
m19e <m19e.dev@gmail.com>
2024-12-29 22:37:32 +0900

    Replace by extracted

--- src/components/TransformerDemo.tsx
+++ src/components/TransformerDemo.tsx
@@ -1,68 +1,13 @@
 'use client'
-import type Konva from 'konva'
-import { useRef } from 'react'
-import { Image, Layer, Stage, Transformer } from 'react-konva'
-import useImage from 'use-image'
+import { Layer, Stage } from 'react-konva'
 
-type ImageData = {
-  src: string
-  width: number
-  height: number
-}
+import { SvgImage } from '@/components/SvgImage'
 
 export const Demo = () => {
-  const imageData: ImageData = {
-    src: 'https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/svg/1f914.svg',
-    width: 200,
-    height: 200,
-  }
-
-  const [image] = useImage(imageData.src)
-  const transformerRef = useRef<Konva.Transformer>(null)
-  const imageRef = useRef<Konva.Image>(null)
-
-  const onSelect = () => {
-    if (imageRef.current) {
-      transformerRef.current?.nodes([imageRef.current])
-    }
-  }
-
   return (
     <Stage className="h-screen w-full bg-slate-200" width={500} height={500}>
       <Layer>
-        {image && (
-          <Image
-            image={image}
-            x={50}
-            y={50}
-            width={imageData.width}
-            height={imageData.height}
-            ref={imageRef}
-            onClick={onSelect}
-            onTap={onSelect}
-            draggable
-          />
-        )}
-        <Transformer
-          ref={transformerRef}
-          keepRatio
-          rotateAnchorOffset={25}
-          anchorSize={8}
-          anchorCornerRadius={4}
-          rotationSnaps={[0]}
-          enabledAnchors={[
-            'top-left',
-            'top-right',
-            'bottom-left',
-            'bottom-right',
-          ]}
-          boundBoxFunc={(oldBox, newBox) => {
-            if (newBox.height < 50 || newBox.width < 50) {
-              return oldBox
-            }
-            return newBox
-          }}
-        />
+        <SvgImage u="1f914" />
       </Layer>
     </Stage>
   )
