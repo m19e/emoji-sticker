af030da3a3ec446f0bfb74248707c325f47f2eeb
m19e <m19e.dev@gmail.com>
2024-12-30 15:28:54 +0900

    Reset emoji-list when reset base-img

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,13 +1,22 @@
 import { atomEffect } from 'jotai-effect'
 import { withHistory } from 'jotai-history'
-import { atomWithReset } from 'jotai/utils'
+import { RESET, atomWithReset } from 'jotai/utils'
 
 export const baseImgUrlAtom = atomWithReset<string | undefined>(undefined)
 
+export const emojiSvgIdsAtom = atomWithReset<{ id: string; u: string }[]>([])
+
 const baseImgUrlHistory = withHistory(baseImgUrlAtom, 2)
-export const revokeEffect = atomEffect((get) => {
+
+/**
+ * baseImgUrlがリセットされた時、
+ * 1. Konva上のすべての絵文字を削除
+ * 2. ベース画像atomに格納していたblobをrevokeする
+ */
+export const revokeEffect = atomEffect((get, set) => {
   const [url, prevUrl] = get(baseImgUrlHistory)
   if (typeof url === 'undefined') {
+    set(emojiSvgIdsAtom, RESET)
     prevUrl && URL.revokeObjectURL(prevUrl)
   }
 })
