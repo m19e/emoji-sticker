c620a8570a3a2c4ba57b596b0e47e573f21e9a42
m19e <m19e.dev@gmail.com>
2024-12-30 15:35:11 +0900

    Set emoji as transformer target

--- src/components/SvgImage.tsx
+++ src/components/SvgImage.tsx
@@ -1,21 +1,27 @@
 'use client'
 import type Konva from 'konva'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { Image, Transformer } from 'react-konva'
 import useImage from 'use-image'
 
 import { getSvgUrl } from '@/tools'
 
-export const SvgImage = ({ u }: { u: string }) => {
+type Props = {
+  u: string
+  selected: boolean
+  onSelect: () => void
+}
+
+export const SvgImage = ({ u, selected, onSelect }: Readonly<Props>) => {
   const [image] = useImage(getSvgUrl(u))
   const transformerRef = useRef<Konva.Transformer>(null)
   const imageRef = useRef<Konva.Image>(null)
 
-  const onSelect = () => {
-    if (imageRef.current) {
+  useEffect(() => {
+    if (selected && imageRef.current) {
       transformerRef.current?.nodes([imageRef.current])
     }
-  }
+  }, [selected])
 
   return (
     <>
@@ -28,30 +34,34 @@ export const SvgImage = ({ u }: { u: string }) => {
           height={100}
           ref={imageRef}
           onClick={onSelect}
+          onDragStart={onSelect}
           onTap={onSelect}
+          onTouchStart={onSelect}
           draggable
         />
       )}
-      <Transformer
-        ref={transformerRef}
-        keepRatio
-        rotateAnchorOffset={25}
-        anchorSize={8}
-        anchorCornerRadius={4}
-        rotationSnaps={[0]}
-        enabledAnchors={[
-          'top-left',
-          'top-right',
-          'bottom-left',
-          'bottom-right',
-        ]}
-        boundBoxFunc={(oldBox, newBox) => {
-          if (newBox.height < 50 || newBox.width < 50) {
-            return oldBox
-          }
-          return newBox
-        }}
-      />
+      {selected && (
+        <Transformer
+          ref={transformerRef}
+          keepRatio
+          rotateAnchorOffset={25}
+          anchorSize={8}
+          anchorCornerRadius={4}
+          rotationSnaps={[0]}
+          enabledAnchors={[
+            'top-left',
+            'top-right',
+            'bottom-left',
+            'bottom-right',
+          ]}
+          boundBoxFunc={(oldBox, newBox) => {
+            if (newBox.height < 50 || newBox.width < 50) {
+              return oldBox
+            }
+            return newBox
+          }}
+        />
+      )}
     </>
   )
 }
