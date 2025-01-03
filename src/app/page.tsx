810c92eac0dd6f86026863c69ed740b336d18adf
m19e <m19e.dev@gmail.com>
2025-01-03 18:37:02 +0900

    Fix entire markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { BaseImage } from '@/components/BaseImage'
 import { DeleteBaseImageButton } from '@/components/DeleteBaseImageButton'
 import { Dropzone } from '@/components/Dropzone'
 import { Picker } from '@/components/EmojiPicker'
@@ -6,11 +5,12 @@ import { Demo } from '@/components/TransformerDemo'
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8">
-        <BaseImage />
-        <Dropzone />
-        <DeleteBaseImageButton />
+    <div className="flex min-h-screen flex-col items-center bg-slate-800 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex h-screen flex-col items-center gap-4">
+        <div className="flex w-full justify-between gap-4">
+          <Dropzone />
+          <DeleteBaseImageButton />
+        </div>
         <Demo />
         <Picker />
       </main>
