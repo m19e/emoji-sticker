ae5c02ee662ed9fe4a51c975b89e7282f85be6f6
m19e <m19e.dev@gmail.com>
2025-01-07 00:49:23 +0900

    Fit canvas to aspect-fix parent

--- src/components/TransformerDemo.tsx
+++ src/components/TransformerDemo.tsx
@@ -1,47 +1,68 @@
 'use client'
 import { useAtomValue } from 'jotai'
-import type Konva from 'konva'
 import { useState } from 'react'
-import { Layer, Stage } from 'react-konva'
+import { Image, Layer, Stage } from 'react-konva'
 
 import { SvgImage } from '@/components/SvgImage'
-import { emojiSvgIdsAtom } from '@/store/atoms'
+import { useImageSize } from '@/hooks/useImageSize'
+import { baseImgUrlAtom, emojiSvgIdsAtom } from '@/store/atoms'
+import { useMeasure } from 'react-use'
+import useImage from 'use-image'
 
 export const Demo = () => {
   const emojis = useAtomValue(emojiSvgIdsAtom)
   const [selectedId, setSelectedId] = useState<string | null>(null)
 
-  const checkUnselect = (
-    e: Konva.KonvaEventObject<MouseEvent> | Konva.KonvaEventObject<TouchEvent>,
-  ) => {
-    const clickedOnEmpty = e.target === e.target.getStage()
-    if (clickedOnEmpty) {
-      setSelectedId(null)
-    }
+  const url = useAtomValue(baseImgUrlAtom)
+  const [dimensions] = useImageSize(url)
+  const [image] = useImage(url ?? '')
+
+  const [ref, { width, height }] = useMeasure<HTMLDivElement>()
+
+  const handleUnselect = () => {
+    setSelectedId(null)
   }
 
   const handleSelect = (id: string) => {
     setSelectedId(id)
   }
 
+  if (dimensions === null) {
+    return null
+  }
+
   return (
-    <Stage
-      className="h-screen w-full bg-slate-200"
-      width={500}
-      height={500}
-      onMouseDown={checkUnselect}
-      onTouchStart={checkUnselect}
-    >
-      <Layer>
-        {emojis.map((e) => (
-          <SvgImage
-            key={e.id}
-            u={e.u}
-            selected={e.id === selectedId}
-            onSelect={() => handleSelect(e.id)}
-          />
-        ))}
-      </Layer>
-    </Stage>
+    <>
+      <pre>{JSON.stringify({ width, height }, null, 4)}</pre>
+      <div
+        ref={ref}
+        className="w-full"
+        style={{
+          aspectRatio: `${dimensions.width} / ${dimensions.height}`,
+        }}
+      >
+        <Stage
+          className="flex-1 bg-slate-200"
+          width={width}
+          height={height}
+          scaleX={width / dimensions.width}
+          scaleY={height / dimensions.height}
+        >
+          <Layer onMouseDown={handleUnselect} onTouchStart={handleUnselect}>
+            <Image image={image} x={0} y={0} />
+          </Layer>
+          <Layer>
+            {emojis.map((e) => (
+              <SvgImage
+                key={e.id}
+                u={e.u}
+                selected={e.id === selectedId}
+                onSelect={() => handleSelect(e.id)}
+              />
+            ))}
+          </Layer>
+        </Stage>
+      </div>
+    </>
   )
 }
