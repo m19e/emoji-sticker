8cca1c514292800bd0448e1b6cdf8612a0212aaa
m19e <m19e.dev@gmail.com>
2025-01-07 18:20:16 +0900

    Relative

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Demo } from '@/components/TransformerDemo'
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center bg-slate-800 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex h-screen flex-col items-center gap-4">
+      <main className="relative flex h-screen flex-col items-center gap-4">
         <div className="flex w-full justify-between gap-4">
           <Dropzone />
           <DeleteBaseImageButton />
