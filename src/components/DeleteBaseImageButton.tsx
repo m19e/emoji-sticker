d42adb35c14608ba4045777e25dad6976d197fb3
m19e <m19e.dev@gmail.com>
2025-01-08 01:24:41 +0900

    style: fix button size

--- src/components/DeleteBaseImageButton.tsx
+++ src/components/DeleteBaseImageButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useResetAtom } from 'jotai/utils'
-import { Trash2 as TrashIcon } from 'lucide-react'
+import { Trash2Icon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { baseImgUrlAtom } from '@/store/atoms'
@@ -9,8 +9,13 @@ export const DeleteBaseImageButton = () => {
   const resetBaseImg = useResetAtom(baseImgUrlAtom)
 
   return (
-    <Button className="text-slate-300" variant="ghost" onClick={resetBaseImg}>
-      <TrashIcon />
+    <Button
+      className="text-slate-300"
+      variant="ghost"
+      size="icon"
+      onClick={resetBaseImg}
+    >
+      <Trash2Icon />
     </Button>
   )
 }
