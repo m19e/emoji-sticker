d83eb8d4158c51a6032ed3d255a8a1cce2015e0e
m19e <m19e.dev@gmail.com>
2025-01-08 14:55:33 +0900

    feat: fix buttons order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <div className="relative flex justify-center bg-slate-600 font-[family-name:var(--font-geist-sans)]">
       <main className="flex h-screen w-96 max-w-full flex-col bg-black sm:w-auto sm:min-w-96">
         <div className="flex justify-between gap-4">
-          <Dropzone />
           <DeleteBaseImageButton />
+          <Dropzone />
         </div>
         <div className="flex h-[calc(100%-72px)] w-full flex-col items-center justify-center bg-slate-400">
           <Demo />
