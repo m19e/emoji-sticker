54dfe61d5a25f56aeab311e4715bbca5117ab6e8
m19e <m19e.dev@gmail.com>
2025-01-08 21:47:46 +0900

    feat: w-full when mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ import { Editor } from '@/components/StickerEditor'
 export default function Home() {
   return (
     <div className="relative flex justify-center bg-slate-600 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex h-screen w-96 max-w-full flex-col bg-black sm:w-auto sm:min-w-96">
+      <main className="flex h-screen w-full flex-col bg-black sm:w-auto sm:min-w-96">
         <div className="flex justify-between gap-4">
           <DeleteBaseImageButton />
           <Dropzone />
         </div>
-        <div className="flex h-[calc(100%-72px)] w-full flex-col items-center justify-center bg-slate-400">
+        <div className="flex h-[calc(100%-72px)] flex-col items-center justify-center bg-slate-400">
           <Editor />
         </div>
         <div className="flex justify-end gap-4">
