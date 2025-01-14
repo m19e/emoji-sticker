3046b539bacc4277fd5b8a2de00c61469cd020b0
m19e <m19e.dev@gmail.com>
2025-01-14 15:47:03 +0900

    ⚰️ delete base-image component file

--- src/components/BaseImage.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { baseImgUrlAtom } from '@/store/atoms'
-import { useAtomValue } from 'jotai'
-
-export const BaseImage = () => {
-  const url = useAtomValue(baseImgUrlAtom)
-
-  return <img src={url} alt={url} />
-}
