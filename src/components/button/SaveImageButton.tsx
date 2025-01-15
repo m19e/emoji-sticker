344376ca31f8f17b777c8b75c0184f6bc1df5716
m19e <m19e.dev@gmail.com>
2025-01-15 23:04:17 +0900

    ✨ Open share-drawer

--- src/components/button/SaveImageButton.tsx
+++ src/components/button/SaveImageButton.tsx
@@ -1,18 +1,19 @@
 'use client'
+import { useSetAtom } from 'jotai'
 import { useResetAtom } from 'jotai/utils'
-import { SaveIcon } from 'lucide-react'
+import { ShareIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
-import { selectedEmojiIdAtom } from '@/store/atoms'
+import { isShareDrawerOpenAtom, selectedEmojiIdAtom } from '@/store/atoms'
 import type { ButtonProps } from '@/types'
 
 export const SaveImageButton = ({ disabled }: ButtonProps) => {
+  const setOpenDrawer = useSetAtom(isShareDrawerOpenAtom)
   const resetSelectedEmojiId = useResetAtom(selectedEmojiIdAtom)
 
   const handleClick = () => {
     resetSelectedEmojiId()
-    // TODO
-    // openShareDialog()
+    setOpenDrawer(true)
   }
 
   return (
@@ -23,7 +24,7 @@ export const SaveImageButton = ({ disabled }: ButtonProps) => {
       onClick={handleClick}
       disabled={disabled}
     >
-      <SaveIcon />
+      <ShareIcon />
     </Button>
   )
 }
