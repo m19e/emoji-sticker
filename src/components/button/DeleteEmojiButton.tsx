08861c0c8d264261044e0232af66fdc03676fc69
m19e <m19e.dev@gmail.com>
2025-01-19 20:00:00 +0900

    ✨ Delete rect-sticker

--- src/components/button/DeleteEmojiButton.tsx
+++ src/components/button/DeleteEmojiButton.tsx
@@ -3,14 +3,20 @@ import { useAtomValue, useSetAtom } from 'jotai'
 import { Trash2Icon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
-import { emojiDatasAtom, selectedStickerIdAtom } from '@/store/atoms'
+import {
+  emojiDatasAtom,
+  rectanglesAtom,
+  selectedStickerIdAtom,
+} from '@/store/atoms'
 
 export const DeleteEmojiButton = () => {
   const selectedStickerId = useAtomValue(selectedStickerIdAtom)
   const setEmojiDatas = useSetAtom(emojiDatasAtom)
+  const setRectangles = useSetAtom(rectanglesAtom)
 
   const handleClick = () => {
     setEmojiDatas((p) => p.filter((e) => e.id !== selectedStickerId))
+    setRectangles((p) => p.filter((r) => r.id !== selectedStickerId))
   }
 
   return (
