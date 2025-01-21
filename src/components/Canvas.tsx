e4215f317af335f51e9d8258a10c9a89b9a41ba6
m19e <m19e.dev@gmail.com>
2025-01-21 21:05:41 +0900

    ♻️ Extract editor canvas component

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,26 +1,77 @@
 'use client'
+import { useAtom, useAtomValue } from 'jotai'
+import { RESET } from 'jotai/utils'
+import type Konva from 'konva'
+import type { RefObject } from 'react'
 import { Image, Layer, Stage } from 'react-konva'
-import useImage from 'use-image'
 
-export const Canvas = () => {
-  const svgUrl =
-    'https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/svg/1f914.svg'
-  const [image] = useImage(svgUrl)
-  const xy = (500 - 100) / 2
+import { Emoji } from '@/components/sticker/Emoji'
+import { Rectangle } from '@/components/sticker/Rectangle'
+import { useAnonymousImage } from '@/hooks/useAnonymousImage'
+import { useImageSize } from '@/hooks/useImageSize'
+import {
+  baseImgUrlAtom,
+  emojiDatasAtom,
+  rectanglesAtom,
+  selectedStickerIdAtom,
+} from '@/store/atoms'
+
+type Props = {
+  ref: RefObject<Konva.Stage | null>
+  width: number
+  height: number
+}
+
+export const Canvas = ({ ref, width, height }: Props) => {
+  const emojis = useAtomValue(emojiDatasAtom)
+  const rects = useAtomValue(rectanglesAtom)
+  const [selectedStickerId, setSelectedStickerId] = useAtom(
+    selectedStickerIdAtom,
+  )
+
+  const url = useAtomValue(baseImgUrlAtom)
+  const [dimensions] = useImageSize(url)
+  const [image] = useAnonymousImage(url ?? '')
+
+  const handleUnselect = () => {
+    setSelectedStickerId(RESET)
+  }
+
+  const handleSelect = (id: string) => {
+    setSelectedStickerId(id)
+  }
 
   return (
-    <Stage width={500} height={500} className="bg-slate-300">
+    <Stage
+      ref={ref}
+      width={width}
+      height={height}
+      scaleX={width / dimensions.width}
+      scaleY={height / dimensions.height}
+    >
+      <Layer onMouseDown={handleUnselect} onTouchStart={handleUnselect}>
+        <Image image={image} x={0} y={0} />
+      </Layer>
       <Layer>
-        {image && (
-          <Image
-            image={image}
-            x={xy}
-            y={xy}
-            width={100}
-            height={100}
-            draggable
+        {emojis.map((e) => (
+          <Emoji
+            key={e.id}
+            u={e.u}
+            selected={e.id === selectedStickerId}
+            onSelect={() => handleSelect(e.id)}
+            x={dimensions.width / 2}
+            y={dimensions.height / 2}
           />
-        )}
+        ))}
+        {rects.map((r) => (
+          <Rectangle
+            key={r.id}
+            selected={r.id === selectedStickerId}
+            onSelect={() => handleSelect(r.id)}
+            x={dimensions.width / 2}
+            y={dimensions.height / 2}
+          />
+        ))}
       </Layer>
     </Stage>
   )
