dc7a99cbd492720acfd43c4eab7613091336b615
m19e <m19e.dev@gmail.com>
2025-01-23 00:15:46 +0900

    ✨ Calc canvas-dimensions with custom hook

--- src/components/StickerEditor.tsx
+++ src/components/StickerEditor.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useAtomValue } from 'jotai'
-import { useMeasure } from 'react-use'
+import { useMeasure, useMedia, useWindowSize } from 'react-use'
 
 import { Canvas } from '@/components/Canvas'
 import { ShareDialog } from '@/components/ShareDialog'
@@ -9,23 +9,31 @@ import { useImageSize } from '@/hooks/useImageSize'
 import { baseImgUrlAtom } from '@/store/atoms'
 import type { Dimensions } from '@/types'
 
-const checkAspectRatio = ({
-  img,
-  parent,
-}: { img: Dimensions; parent: Dimensions }): { isFullWidth: boolean } => {
+const useCanvasDimensions = (img: Dimensions): { isFullWidth: boolean } => {
+  const { width, height } = useWindowSize({ initialWidth: 0, initialHeight: 0 })
+  const isDesktop = useMedia('(min-width: 640px)')
+  const desktopContentsWidth = 384
+  const marginY = 72
+
+  const canvasWidth = isDesktop ? desktopContentsWidth : width
+  const canvasHeight = height - marginY
+
   if (img.width === img.height) {
     return { isFullWidth: true }
   }
+
   const isPortraitImg = img.width < img.height
 
   if (isPortraitImg) {
     // h-fullで横幅あふれる場合を計算する
-    const clientWidth = parent.height * (img.width / img.height)
-    const isOverflowX = clientWidth >= parent.width
+    const clientWidth = canvasHeight * (img.width / img.height)
+    const isOverflowX = clientWidth >= canvasWidth
     return { isFullWidth: isOverflowX }
   }
 
-  return { isFullWidth: false }
+  const clientHeight = canvasWidth * (img.height / img.width)
+  const isOverflowY = clientHeight >= canvasHeight
+  return { isFullWidth: !isOverflowY }
 }
 
 export const Editor = () => {
@@ -34,7 +42,7 @@ export const Editor = () => {
   const [ref, { width, height }] = useMeasure<HTMLDivElement>()
   const [canvasRef, { save }] = useCanvasData()
 
-  const isFullWidth = dimensions.width >= dimensions.height
+  const { isFullWidth } = useCanvasDimensions(dimensions)
 
   const handleSave = () => {
     const ratio = isFullWidth
