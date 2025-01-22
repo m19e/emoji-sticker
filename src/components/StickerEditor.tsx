3b11b6d86b5652996de02271cf36cf10fedcb6b7
m19e <m19e.dev@gmail.com>
2025-01-23 00:19:25 +0900

    ♻️ Extract hook

--- src/components/StickerEditor.tsx
+++ src/components/StickerEditor.tsx
@@ -1,40 +1,13 @@
 'use client'
 import { useAtomValue } from 'jotai'
-import { useMeasure, useMedia, useWindowSize } from 'react-use'
+import { useMeasure } from 'react-use'
 
 import { Canvas } from '@/components/Canvas'
 import { ShareDialog } from '@/components/ShareDialog'
 import { useCanvasData } from '@/hooks/useCanvasData'
+import { useCanvasDimensions } from '@/hooks/useCanvasDimensions'
 import { useImageSize } from '@/hooks/useImageSize'
 import { baseImgUrlAtom } from '@/store/atoms'
-import type { Dimensions } from '@/types'
-
-const useCanvasDimensions = (img: Dimensions): { isFullWidth: boolean } => {
-  const { width, height } = useWindowSize({ initialWidth: 0, initialHeight: 0 })
-  const isDesktop = useMedia('(min-width: 640px)')
-  const desktopContentsWidth = 384
-  const marginY = 72
-
-  const canvasWidth = isDesktop ? desktopContentsWidth : width
-  const canvasHeight = height - marginY
-
-  if (img.width === img.height) {
-    return { isFullWidth: true }
-  }
-
-  const isPortraitImg = img.width < img.height
-
-  if (isPortraitImg) {
-    // h-fullで横幅あふれる場合を計算する
-    const clientWidth = canvasHeight * (img.width / img.height)
-    const isOverflowX = clientWidth >= canvasWidth
-    return { isFullWidth: isOverflowX }
-  }
-
-  const clientHeight = canvasWidth * (img.height / img.width)
-  const isOverflowY = clientHeight >= canvasHeight
-  return { isFullWidth: !isOverflowY }
-}
 
 export const Editor = () => {
   const url = useAtomValue(baseImgUrlAtom)
---
+++ src/hooks/useCanvasDimensions.ts
@@ -0,0 +1,32 @@
+import type { Dimensions } from '@/types'
+import { useMedia, useWindowSize } from 'react-use'
+
+export const useCanvasDimensions = (
+  img: Dimensions,
+): { isFullWidth: boolean } => {
+  const { width, height } = useWindowSize({ initialWidth: 0, initialHeight: 0 })
+  const isDesktop = useMedia('(min-width: 640px)')
+  const desktopContentsWidth = 384
+  const marginY = 72
+
+  const canvasWidth = isDesktop ? desktopContentsWidth : width
+  const canvasHeight = height - marginY
+
+  if (img.width === img.height) {
+    return { isFullWidth: true }
+  }
+
+  const isPortraitImg = img.width < img.height
+
+  if (isPortraitImg) {
+    // h-fullで横幅あふれる場合を計算する
+    const clientWidth = canvasHeight * (img.width / img.height)
+    const isOverflowX = clientWidth >= canvasWidth
+    return { isFullWidth: isOverflowX }
+  }
+
+  // w-fullで縦幅あふれる場合を計算する
+  const clientHeight = canvasWidth * (img.height / img.width)
+  const isOverflowY = clientHeight >= canvasHeight
+  return { isFullWidth: !isOverflowY }
+}
