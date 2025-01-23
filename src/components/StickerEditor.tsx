a5e3f2c42036e1ec47cab1b4ee43d816a3ce885c
m19e <m19e.dev@gmail.com>
2025-01-24 00:50:06 +0900

    🐛 Set width/height for iphone bug

--- src/components/StickerEditor.tsx
+++ src/components/StickerEditor.tsx
@@ -8,6 +8,7 @@ import { useCanvasData } from '@/hooks/useCanvasData'
 import { useCanvasDimensions } from '@/hooks/useCanvasDimensions'
 import { useImageSize } from '@/hooks/useImageSize'
 import { baseImgUrlAtom } from '@/store/atoms'
+import { type CSSProperties, useMemo } from 'react'
 
 export const Editor = () => {
   const url = useAtomValue(baseImgUrlAtom)
@@ -24,14 +25,31 @@ export const Editor = () => {
     save(ratio)
   }
 
+  const containerStyle = useMemo(() => {
+    const aspectRatio = `${dimensions.width} / ${dimensions.height}`
+    const style: CSSProperties = isFullWidth
+      ? {
+          aspectRatio,
+          width: '100%',
+          height: `calc(${height} * ${dimensions.height} / ${dimensions.width})`,
+          maxHeight: 'fit-content',
+        }
+      : {
+          aspectRatio,
+          width: `calc(${width} * ${dimensions.width} / ${dimensions.height})`,
+          height: '100%',
+          maxWidth: 'fit-content',
+        }
+
+    return style
+  }, [dimensions, isFullWidth, width, height])
+
   return (
     <>
       <div
         ref={ref}
-        className={isFullWidth ? 'max-h-fit w-full' : 'h-full max-w-fit'}
-        style={{
-          aspectRatio: `${dimensions.width} / ${dimensions.height}`,
-        }}
+        // className={isFullWidth ? 'max-h-fit w-full' : 'h-full max-w-fit'}
+        style={containerStyle}
       >
         <Canvas
           ref={canvasRef}
