77ac173977a5555bb8ec7886ba7ae9c4e2eafc5d
m19e <m19e.dev@gmail.com>
2025-01-28 23:57:36 +0900

    ✨ Show emoji-picker in drawer

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -4,6 +4,12 @@ import { useAtom, useSetAtom } from 'jotai'
 import dynamic from 'next/dynamic'
 import { v4 } from 'uuid'
 
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+} from '@/components/ui/drawer'
 import { DEFAULT_PREVIEW_CONFIG, EPR_CATEGORIES_JA } from '@/constants'
 import {
   emojiDatasAtom,
@@ -28,16 +34,26 @@ export const Picker = () => {
   }
 
   return (
-    <EmojiPicker
-      open={open}
-      className="!fixed bottom-0"
-      emojiStyle={EmojiStyle.TWITTER}
-      categories={EPR_CATEGORIES_JA}
-      previewConfig={DEFAULT_PREVIEW_CONFIG}
-      skinTonesDisabled
-      autoFocusSearch={false}
-      searchPlaceholder="検索"
-      onEmojiClick={handleClick}
-    />
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerContent className="mx-auto w-full items-center bg-slate-600 sm:w-min">
+        <DrawerHeader className="w-[382px] max-w-full">
+          <DrawerTitle className="text-center text-gray-200">
+            絵文字を選択
+          </DrawerTitle>
+        </DrawerHeader>
+        <div className="overflow-x-hidden p-4 pt-0">
+          <EmojiPicker
+            className="!flex"
+            emojiStyle={EmojiStyle.TWITTER}
+            categories={EPR_CATEGORIES_JA}
+            previewConfig={DEFAULT_PREVIEW_CONFIG}
+            skinTonesDisabled
+            autoFocusSearch={false}
+            searchPlaceholder="検索"
+            onEmojiClick={handleClick}
+          />
+        </div>
+      </DrawerContent>
+    </Drawer>
   )
 }
