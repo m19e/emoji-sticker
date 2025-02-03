4f21c7b2060085214e54ce46955b31fb4071593b
m19e <m19e.dev@gmail.com>
2025-02-03 21:40:22 +0900

    🗑️ Check window-size on iphone

---
+++ src/components/iPhoneTest.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useWindowSize } from 'react-use'
+
+export const Test = () => {
+  const { width: winWidth, height: winHeight } = useWindowSize({
+    initialWidth: 0,
+    initialHeight: 0,
+  })
+
+  return (
+    <div className="grid min-h-screen w-full bg-slate-600">
+      <pre className="place-self-center rounded-md bg-white p-4">
+        {JSON.stringify({ winWidth, winHeight }, null, 2)}
+      </pre>
+    </div>
+  )
+}
