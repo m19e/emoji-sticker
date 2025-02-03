6e2a1bbc7ba00da7a5f749fd3e2be522137acb5f
m19e <m19e.dev@gmail.com>
2025-02-03 22:11:01 +0900

    ✨ Add hook for calc canvas size

--- src/hooks/useCanvasDimensions.ts
+++ src/hooks/useCanvasDimensions.ts
@@ -3,9 +3,12 @@ import { useMedia, useWindowSize } from 'react-use'
 import { CANVAS_MARGIN_Y, DESKTOP_CONTENTS_WIDTH } from '@/constants'
 import type { Dimensions } from '@/types'
 
-export const useCanvasDimensions = (
-  img: Dimensions,
-): { isFullWidth: boolean } => {
+type Return = {
+  isFullWidth: boolean
+}
+
+// Canvasの高さ・幅をちゃんと計算する
+export const useCanvasDimensions = (img: Dimensions): Return => {
   const { width, height } = useWindowSize({ initialWidth: 0, initialHeight: 0 })
   const isDesktop = useMedia('(min-width: 640px)')
 
@@ -29,4 +32,26 @@ export const useCanvasDimensions = (
   const clientHeight = canvasWidth * (img.height / img.width)
   const isOverflowY = clientHeight >= canvasHeight
   return { isFullWidth: !isOverflowY }
+}
+
+export const useCanvasSize = (img: Dimensions) => {
+  const { width, height } = useWindowSize({ initialWidth: 0, initialHeight: 0 })
+  const isDesktop = useMedia('(min-width: 640px)')
+
+  const fullWidth = isDesktop ? DESKTOP_CONTENTS_WIDTH : width
+  const fullHeight = height - CANVAS_MARGIN_Y
+
+  const { isFullWidth } = useCanvasDimensions(img)
+
+  const style = isFullWidth
+    ? {
+        width: fullWidth,
+        height: fullWidth * (img.height / img.width),
+      }
+    : {
+        height: fullHeight,
+        width: fullHeight * (img.width / img.height),
+      }
+
+  return { isFullWidth, canvas: style }
 }
