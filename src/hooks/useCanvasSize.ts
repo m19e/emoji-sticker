c404e711747d638ee419a1305cffd40740d8bc51
m19e <m19e.dev@gmail.com>
2025-02-04 19:15:19 +0900

    ♻️ Shift calc process from old hook

--- src/hooks/useCanvasSize.ts
+++ src/hooks/useCanvasSize.ts
@@ -1,26 +1,61 @@
+import { useMemo } from 'react'
 import { useMedia, useWindowSize } from 'react-use'
 
 import { CANVAS_MARGIN_Y, DESKTOP_CONTENTS_WIDTH } from '@/constants'
-import { useCanvasDimensions } from '@/hooks/useCanvasDimensions'
 import type { Dimensions } from '@/types'
+
+type Args = {
+  img: Dimensions
+  canvas: Dimensions
+}
+
+const getIsFullWidth = ({ img, canvas }: Args) => {
+  if (img.width === img.height) {
+    return { isFullWidth: true }
+  }
+
+  const isPortraitImg = img.width < img.height
+
+  // h-fullの場合にwidthがあふれる場合を計算する
+  if (isPortraitImg) {
+    const clientWidth = canvas.height * (img.width / img.height)
+    const isOverflowX = clientWidth >= canvas.width
+    return { isFullWidth: isOverflowX }
+  }
+
+  // w-fullの場合にheightがあふれる場合を計算する
+  const clientHeight = canvas.width * (img.height / img.width)
+  const isOverflowY = clientHeight >= canvas.height
+  return { isFullWidth: !isOverflowY }
+}
 
 export const useCanvasSize = (img: Dimensions) => {
   const { width, height } = useWindowSize({ initialWidth: 0, initialHeight: 0 })
   const isDesktop = useMedia('(min-width: 640px)')
 
-  const fullWidth = isDesktop ? DESKTOP_CONTENTS_WIDTH : width
-  const fullHeight = height - CANVAS_MARGIN_Y
+  const maxWidth = isDesktop ? DESKTOP_CONTENTS_WIDTH : width
+  const maxHeight = height - CANVAS_MARGIN_Y
 
-  const { isFullWidth } = useCanvasDimensions(img)
+  const { isFullWidth } = useMemo(
+    () =>
+      getIsFullWidth({
+        img,
+        canvas: {
+          width: maxWidth,
+          height: maxHeight,
+        },
+      }),
+    [img, maxWidth, maxHeight],
+  )
 
   const style = isFullWidth
     ? {
-        width: fullWidth,
-        height: fullWidth * (img.height / img.width),
+        width: maxWidth,
+        height: maxWidth * (img.height / img.width),
       }
     : {
-        height: fullHeight,
-        width: fullHeight * (img.width / img.height),
+        height: maxHeight,
+        width: maxHeight * (img.width / img.height),
       }
 
   return { isFullWidth, canvas: style }
