a4d236d1da97d4b1a969a2e6be208a51b098cb97
m19e <m19e.dev@gmail.com>
2025-02-08 23:40:07 +0900

    💄 Set bg-color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import { OpenPickerButton } from '@/components/button/OpenPickerButton'
 
 export default function Home() {
   return (
-    <div className="relative flex justify-center bg-slate-900 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex h-dvh w-full flex-col sm:w-96">
+    <div className="relative flex justify-center bg-slate-600 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex h-dvh w-full flex-col bg-slate-950 shadow sm:w-96">
         <Header />
         <div className="flex h-[calc(100%-72px)] flex-col items-center justify-center overflow-hidden bg-slate-400">
           <Editor />
