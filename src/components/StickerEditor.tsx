f1ecb03bd8a30840d33765d4029c32e865cf8c2e
m19e <m19e.dev@gmail.com>
2025-02-09 15:35:34 +0900

    ♻️ Remove needless hooks, props, handler

--- src/components/StickerEditor.tsx
+++ src/components/StickerEditor.tsx
@@ -1,37 +1,14 @@
 'use client'
-import { useAtomValue } from 'jotai'
 
 import { Canvas } from '@/components/Canvas'
 import { ShareDialog } from '@/components/ShareDialog'
-import { useCanvasData } from '@/hooks/useCanvasData'
-import { useCanvasSize } from '@/hooks/useCanvasSize'
-import { useImageSize } from '@/hooks/useImageSize'
-import { baseImgUrlAtom } from '@/store/atoms'
 
+// TODO このファイルを削除
 export const Editor = () => {
-  const url = useAtomValue(baseImgUrlAtom)
-  const [imgSize] = useImageSize(url)
-  const { save, share } = useCanvasData()
-
-  const {
-    fullWidth,
-    canvas: { width, height },
-  } = useCanvasSize(imgSize)
-
-  const handleSave = () => {
-    const ratio = fullWidth ? imgSize.width / width : imgSize.height / height
-    save(ratio)
-  }
-
-  const handleShare = async () => {
-    const ratio = fullWidth ? imgSize.width / width : imgSize.height / height
-    await share(ratio)
-  }
-
   return (
     <>
-      <Canvas width={width} height={height} imgSize={imgSize} />
-      <ShareDialog onSave={handleSave} onShare={handleShare} />
+      <Canvas />
+      <ShareDialog />
     </>
   )
 }
