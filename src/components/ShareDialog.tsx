0ad7fe06e2a2bbd62a69e592201da7fbb1f39e8e
m19e <m19e.dev@gmail.com>
2025-02-10 18:13:52 +0900

    ⚰️ Remove share-dialog when desktop

--- src/components/ShareDialog.tsx
+++ src/components/ShareDialog.tsx
@@ -3,30 +3,18 @@ import { useAtom } from 'jotai'
 import { SaveIcon, Share2Icon } from 'lucide-react'
 
 import { useCanvasData } from '@/hooks/useCanvasData'
-import { useMediaQuery } from '@/hooks/useMediaQuery'
 import { isShareDialogOpenAtom } from '@/store/atoms'
 
 import { Button } from '@/components/ui/button'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog'
 import { Drawer, DrawerContent, DrawerFooter } from '@/components/ui/drawer'
-
-type Props = {
-  onSave: () => void
-  onShare: () => Promise<void>
-}
 
 // TODO descriptionに展開できる説明UIを作る？
 // TODO デスクトップ時dropdown menuにする？
+// TODO ShareActions削除
+// TODO コメント削除
+// TODO Rename
 export const ShareDialog = () => {
   const [open, setOpen] = useAtom(isShareDialogOpenAtom)
-  const { isDesktop } = useMediaQuery()
   const { save, share } = useCanvasData()
 
   const handleSave = () => {
@@ -39,62 +27,28 @@ export const ShareDialog = () => {
     setOpen(false)
   }
 
-  if (isDesktop) {
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="border-none bg-slate-800 sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle className="text-slate-100">
-              画像を保存・共有
-            </DialogTitle>
-            <DialogDescription className="text-muted-foreground text-sm">
-              ※共有から選択すると直接ツイートできます
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <ShareActions onSave={handleSave} onShare={handleShare} />
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    )
-  }
-
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerContent className="border-none bg-slate-800">
-        {/* <DrawerHeader className="text-left">
-          <DrawerTitle className="text-slate-100">画像を保存・共有</DrawerTitle>
-          <DrawerDescription>
-            ※共有から選択すると直接ツイートできます
-          </DrawerDescription>
-        </DrawerHeader> */}
         <DrawerFooter className="pt-4 pb-8">
-          <ShareActions onSave={handleSave} onShare={handleShare} />
+          <Button
+            className="flex justify-between border-none bg-slate-700 px-4 font-bold text-lg text-slate-100"
+            size="lg"
+            onClick={handleSave}
+          >
+            保存する
+            <SaveIcon />
+          </Button>
+          <Button
+            className="flex justify-between border-none bg-slate-700 px-4 font-bold text-lg text-slate-100"
+            size="lg"
+            onClick={handleShare}
+          >
+            共有する
+            <Share2Icon />
+          </Button>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
-  )
-}
-
-const ShareActions = ({ onSave, onShare }: Props) => {
-  return (
-    <>
-      <Button
-        className="flex justify-between border-none bg-slate-700 px-4 font-bold text-lg text-slate-100"
-        size="lg"
-        onClick={onSave}
-      >
-        保存する
-        <SaveIcon />
-      </Button>
-      <Button
-        className="flex justify-between border-none bg-slate-700 px-4 font-bold text-lg text-slate-100"
-        size="lg"
-        onClick={onShare}
-      >
-        共有する
-        <Share2Icon />
-      </Button>
-    </>
   )
 }
