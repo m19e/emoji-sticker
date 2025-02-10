253d62f4af4a9067c89914b1c8bec068bd8c7ad6
m19e <m19e.dev@gmail.com>
2025-02-10 18:14:38 +0900

    ✨ Show share dropdown-menu when desktop

--- src/components/button/OpenShareDialogButton.tsx
+++ src/components/button/OpenShareDialogButton.tsx
@@ -1,20 +1,59 @@
 'use client'
 import { useSetAtom } from 'jotai'
 import { useResetAtom } from 'jotai/utils'
-import { ShareIcon } from 'lucide-react'
+import { SaveIcon, Share2Icon, ShareIcon } from 'lucide-react'
 
+import { useCanvasData } from '@/hooks/useCanvasData'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 import { isShareDialogOpenAtom, selectedStickerIdAtom } from '@/store/atoms'
 import type { ButtonProps } from '@/types'
 
 import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
+// TODO Rename
 export const OpenShareDialogButton = ({ disabled }: ButtonProps) => {
   const setOpenDrawer = useSetAtom(isShareDialogOpenAtom)
   const resetSelectedStickerId = useResetAtom(selectedStickerIdAtom)
 
+  const { isDesktop } = useMediaQuery()
+  const { save, share } = useCanvasData()
+
   const handleClick = () => {
     resetSelectedStickerId()
     setOpenDrawer(true)
+  }
+
+  if (isDesktop) {
+    return (
+      <DropdownMenu onOpenChange={resetSelectedStickerId}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            className="text-slate-300"
+            variant="ghost"
+            size="icon"
+            disabled={disabled}
+          >
+            <ShareIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="border-none bg-slate-800 text-slate-300">
+          <DropdownMenuItem className="justify-between" onClick={save}>
+            保存する
+            <SaveIcon />
+          </DropdownMenuItem>
+          <DropdownMenuItem className="justify-between" onClick={share}>
+            共有する
+            <Share2Icon />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
   }
 
   return (
