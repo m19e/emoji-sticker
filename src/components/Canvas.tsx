e0d85e61ad8bfb295cad177485872e0ad2d61446
m19e <m19e.dev@gmail.com>
2025-02-12 15:38:12 +0900

    🚧 Show dropzone on canvas if base-image not loaded

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,30 +1,39 @@
 'use client'
+import { useAtomValue } from 'jotai'
 import { Stage } from 'react-konva'
 
 import { useBaseImageSize } from '@/hooks/useBaseImageSize'
 import { useCanvasData } from '@/hooks/useCanvasData'
 import { useCanvasSize } from '@/hooks/useCanvasSize'
+import { isBaseImgLoadedAtom } from '@/store/atoms'
 
+import { Dropzone } from '@/components/Dropzone'
 import { BaseImageLayer } from '@/components/layer/BaseImage'
 import { StickerLayer } from '@/components/layer/Sticker'
 
 export const Canvas = () => {
+  const isLoaded = useAtomValue(isBaseImgLoadedAtom)
+
   const { ref } = useCanvasData()
   const {
     canvas: { width, height },
   } = useCanvasSize()
   const { imgSize } = useBaseImageSize()
 
-  return (
-    <Stage
-      ref={ref}
-      width={width}
-      height={height}
-      scaleX={width / imgSize.width}
-      scaleY={height / imgSize.height}
-    >
-      <BaseImageLayer />
-      <StickerLayer img={imgSize} />
-    </Stage>
-  )
+  if (isLoaded) {
+    return (
+      <Stage
+        ref={ref}
+        width={width}
+        height={height}
+        scaleX={width / imgSize.width}
+        scaleY={height / imgSize.height}
+      >
+        <BaseImageLayer />
+        <StickerLayer img={imgSize} />
+      </Stage>
+    )
+  }
+
+  return <Dropzone />
 }
--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,17 +1,13 @@
 'use client'
 import { useAtom, useSetAtom } from 'jotai'
-import { Image as ImageIcon } from 'lucide-react'
 import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 
 import { DROPZONE_ACCEPT_FILE } from '@/constants'
 import { baseImgUrlAtom } from '@/store/atoms'
 import { revokeEffect } from '@/store/effects'
-import type { ButtonProps } from '@/types'
 
-import { Button } from '@/components/ui/button'
-
-export const Dropzone = ({ disabled }: ButtonProps) => {
+export const Dropzone = () => {
   useAtom(revokeEffect)
   const setUrl = useSetAtom(baseImgUrlAtom)
 
@@ -34,15 +30,18 @@ export const Dropzone = ({ disabled }: ButtonProps) => {
   // TODO ヘッダーからcanvasの位置に移動
 
   return (
-    <Button
+    <div
       {...getRootProps()}
-      className="text-slate-300"
-      variant="ghost"
-      size="icon"
-      disabled={disabled}
+      className="aspect-[2/1] w-2/3 min-w-80 rounded-3xl border border-slate-500 bg-slate-800 p-2 shadow sm:p-4"
     >
       <input {...getInputProps()} />
-      <ImageIcon />
-    </Button>
+      <div className="grid h-full w-full place-items-center rounded-xl border-2 border-slate-500 border-dashed">
+        <div className="flex flex-col items-center font-bold text-slate-300">
+          <span>タップ / クリック</span>
+          <span className="font-medium text-sm">または</span>
+          <span>ドロップして画像を追加</span>
+        </div>
+      </div>
+    </div>
   )
 }
