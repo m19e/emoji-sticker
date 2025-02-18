afc859b584ba4cd9ed124b3c92f8ed484af71006
m19e <m19e.dev@gmail.com>
2025-02-18 20:39:35 +0900

    💄 Cover all

--- src/components/Unloaded.tsx
+++ src/components/Unloaded.tsx
@@ -5,5 +5,9 @@ import { Dropzone } from '@/components/Dropzone'
 // TODO 免責事項
 // TODO 各種リンク(SNS, github)
 export const Unloaded = () => {
-  return <Dropzone />
+  return (
+    <div className="h-full w-full">
+      <Dropzone />
+    </div>
+  )
 }
