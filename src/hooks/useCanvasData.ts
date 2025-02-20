d69c5b1c7c60bf7107afc1be0dc46b19a016b66d
m19e <m19e.dev@gmail.com>
2025-02-20 14:50:27 +0900

    ⚗️ Fix output mime-type for reduce image size

--- src/hooks/useCanvasData.ts
+++ src/hooks/useCanvasData.ts
@@ -6,12 +6,18 @@ import { toast } from 'sonner'
 import { useCanvasSize } from '@/hooks/useCanvasSize'
 import { canvasRefAtom } from '@/store/atoms'
 
+// FIXME => constants.ts
+const OUTPUT_MIME_TYPE = 'image/jpeg'
+
 type Return = {
   ref: (ref: Konva.Stage) => void
   save: () => void
   share: () => Promise<void>
 }
 
+// FIXME pixelRatio: 画像が大きくなりすぎる
+// FIXME => 縦横比は正しいがjpeg入力→png出力の場合に圧縮されずに大きくなる
+// FIXME => 出力形式を.jpgに固定
 export const useCanvasData = (): Return => {
   const [canvasRef, setCanvasRef] = useAtom(canvasRefAtom)
   const { pixelRatio } = useCanvasSize()
@@ -27,11 +33,14 @@ export const useCanvasData = (): Return => {
 
   const getFileName = () => {
     const ts = Date.now().toString(16)
-    return `censored-${ts}.png`
+    return `censored-${ts}.jpg`
   }
 
   const save = () => {
-    const uri = canvasRef?.toDataURL({ pixelRatio })
+    const uri = canvasRef?.toDataURL({
+      pixelRatio,
+      mimeType: OUTPUT_MIME_TYPE,
+    })
 
     if (uri) {
       const fileName = getFileName()
@@ -56,7 +65,10 @@ export const useCanvasData = (): Return => {
     if (!isSupported()) {
       return
     }
-    const blob = (await canvasRef?.toBlob({ pixelRatio })) as Blob
+    const blob = (await canvasRef?.toBlob({
+      pixelRatio,
+      mimeType: OUTPUT_MIME_TYPE,
+    })) as Blob
     const file = new File([blob], getFileName(), { type: blob.type })
 
     const text = '絵文字ステッカー！'
