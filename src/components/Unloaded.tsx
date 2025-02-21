9a3ef81ebcca59a6c373bf20c5fccc2eab2225e7
m19e <m19e.dev@gmail.com>
2025-02-22 06:57:56 +0900

    💡 Add todo comment about dark-mode

--- src/components/Unloaded.tsx
+++ src/components/Unloaded.tsx
@@ -4,6 +4,7 @@ import { Dropzone } from '@/components/Dropzone'
 // TODO アプリのロゴ
 // TODO 免責事項
 // TODO 各種リンク(SNS, github)
+// TODO ダークモード対応
 export const Unloaded = () => {
   return (
     <div className="grid h-full w-full place-items-center">
