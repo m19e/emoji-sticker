2fde0e7c61293e51505b59f51a208406f5e54602
m19e <m19e.dev@gmail.com>
2025-02-27 23:44:46 +0900

    💡 Add todo comment

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -8,6 +8,7 @@ import { useCanvasSize } from '@/hooks/useCanvasSize'
 import { BaseImageLayer } from '@/components/layer/BaseImage'
 import { StickerLayer } from '@/components/layer/Sticker'
 
+// TODO コンテキストメニューを無効化
 export const Canvas = () => {
   const { ref } = useCanvasData()
   const {
