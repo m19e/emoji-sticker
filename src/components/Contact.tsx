435893dfc18c3cfaee7adac83dc72bb40baf4916
m19e <m19e.dev@gmail.com>
2025-03-14 15:52:51 +0900

    ✨ Contact with links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,28 @@
+import { Separator } from '@/components/ui/separator'
+
 // TODO リンクwithアイコン
 export const Contact = () => {
-  return <div>©️2025 m19e | github twitter</div>
+  return (
+    <div className="flex gap-2 p-1 text-sm text-zinc-200">
+      <span>©2025</span>
+      <span>m19e</span>
+      <Separator orientation="vertical" className="h-5 w-[1.5px] bg-zinc-400" />
+      <a
+        href="https://github.com/m19e"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline hover:text-zinc-400"
+      >
+        github
+      </a>
+      <a
+        href="https://twitter.com/Versas_me"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline hover:text-zinc-400"
+      >
+        twitter(x)
+      </a>
+    </div>
+  )
 }
