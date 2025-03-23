6560dcb014fd3a6ef32d0c117b22ad2356c4c68a
m19e <m19e.dev@gmail.com>
2025-03-23 14:17:49 +0900

    💄 Show app logo (demo)

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,10 +2,13 @@ import { Separator } from '@/components/ui/separator'
 
 export const Contact = () => {
   return (
-    <div className="flex gap-2 p-1 text-sm text-zinc-200">
+    <div className="flex gap-2 p-1 text-xs text-zinc-200 sm:text-sm">
       <span>©2025</span>
       <span>m19e</span>
-      <Separator orientation="vertical" className="h-5 w-[1.5px] bg-zinc-400" />
+      <Separator
+        orientation="vertical"
+        className="h-4 w-[1.5px] bg-zinc-400 sm:h-5"
+      />
       <a
         href="https://github.com/m19e"
         target="_blank"
--- src/components/Unloaded.tsx
+++ src/components/Unloaded.tsx
@@ -2,16 +2,18 @@ import { Contact } from '@/components/Contact'
 import { Dropzone } from '@/components/Dropzone'
 
 // TODO アプリのロゴ,フォント決め
-// 絵文字
-// ステッカー！
-// みたいなロゴ
 // TODO 免責事項
 export const Unloaded = () => {
   return (
     <div className="grid h-full w-full place-items-center py-4">
       <div />
       <Dropzone />
-      <Contact />
+      <div className="flex flex-col items-center gap-2">
+        <span className="!font-rampart text-2xl sm:text-3xl">
+          絵文字ステッカー！
+        </span>
+        <Contact />
+      </div>
     </div>
   )
 }
