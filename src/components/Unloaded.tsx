b0a2548de6b8401f7d1df23e34a5abb9041f1e2d
m19e <m19e.dev@gmail.com>
2025-03-23 22:26:23 +0900

    💄 Justify-end, fix spacing

--- src/components/Unloaded.tsx
+++ src/components/Unloaded.tsx
@@ -5,18 +5,16 @@ import { Dropzone } from '@/components/Dropzone'
 // TODO 免責事項
 export const Unloaded = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center">
-      <div className="flex flex-1 flex-col justify-end">
+    <div className="flex h-full w-full flex-col items-center justify-end gap-8 pb-8">
+      <div className="flex flex-col justify-end">
         <Dropzone />
       </div>
-      <div className="flex flex-1 flex-col items-center justify-evenly">
-        <div className="logo flex flex-col text-6xl">
-          <span>絵文字</span>
-          <span>ステッ</span>
-          <span>カー！</span>
-        </div>
-        <Contact />
+      <div className="logo flex flex-col text-7xl">
+        <span>絵文字</span>
+        <span>ステッ</span>
+        <span>カー！</span>
       </div>
+      <Contact />
     </div>
   )
 }
