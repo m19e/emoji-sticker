1d544f780be7bc7d5b37bcd6b22c70b84fcb6bbd
m19e <m19e.dev@gmail.com>
2025-03-25 15:18:51 +0900

    💡 Add todo for about-dialog

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -6,6 +6,7 @@ import { isBaseImgLoadedAtom } from '@/store/atoms'
 import { DeleteBaseImageButton } from '@/components/button/DeleteBaseImageButton'
 import { ShareButton } from '@/components/button/ShareButton'
 
+// TODO Aboutダイアログ表示ボタンを設置
 export const Header = () => {
   const isLoaded = useAtomValue(isBaseImgLoadedAtom)
 
--- src/components/Unloaded.tsx
+++ src/components/Unloaded.tsx
@@ -3,6 +3,7 @@ import { Dropzone } from '@/components/Dropzone'
 
 // TODO アプリのロゴ,フォント決め
 // TODO 免責事項
+// TODO ロゴと免責事項をページアクセス時にダイアログで表示
 export const Unloaded = () => {
   return (
     <div className="flex h-full max-h-[680px] flex-col items-center justify-end gap-8 pb-8">
