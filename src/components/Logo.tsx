39b4f604af2be9983b651566e7e61fcb23b5134d
m19e <m19e.dev@gmail.com>
2025-03-25 15:33:43 +0900

    ♻️ Extract logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+export const Logo = () => {
+  return (
+    <div className="logo flex flex-col text-7xl">
+      <span>絵文字</span>
+      <span>ステッ</span>
+      <span>カー！</span>
+    </div>
+  )
+}
--- src/components/Unloaded.tsx
+++ src/components/Unloaded.tsx
@@ -1,18 +1,16 @@
 import { Contact } from '@/components/Contact'
 import { Dropzone } from '@/components/Dropzone'
+import { Logo } from '@/components/Logo'
 
 // TODO アプリのロゴ,フォント決め
 // TODO 免責事項
 // TODO ロゴと免責事項をページアクセス時にダイアログで表示
+// TODO Logoコンポーネントとして切り出し
 export const Unloaded = () => {
   return (
     <div className="flex h-full max-h-[680px] flex-col items-center justify-end gap-8 pb-8">
       <Dropzone />
-      <div className="logo flex flex-col text-7xl">
-        <span>絵文字</span>
-        <span>ステッ</span>
-        <span>カー！</span>
-      </div>
+      <Logo />
       <Contact />
     </div>
   )
