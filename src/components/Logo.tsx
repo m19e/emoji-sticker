d227a17887b7cb0049c27affa14f8d296b51589a
m19e <m19e.dev@gmail.com>
2025-04-03 14:59:33 +0900

    💄 Fix logo font-size

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 export const Logo = () => {
   return (
-    <div className="logo flex flex-col text-7xl">
+    <div className="logo flex flex-col text-6xl">
       <span>絵文字</span>
       <span>ステッ</span>
       <span>カー！</span>
