102628a9a04e854ebbea23fc5df6aac9ddeb4218
m19e <m19e.dev@gmail.com>
2025-04-06 13:24:07 +0900

    💡 Add todo

--- src/components/sticker/Rectangle.tsx
+++ src/components/sticker/Rectangle.tsx
@@ -6,6 +6,7 @@ import { Rect, Transformer } from 'react-konva'
 import { StickerSnap } from '@/constants'
 import type { StickerProps } from '@/types'
 
+// TODO モバイル時だけスナップを角でなく中央に
 export const Rectangle = ({
   selected,
   onSelect,
