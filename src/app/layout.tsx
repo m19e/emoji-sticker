a08389d5d63a6ad0ddc27fab832fa098395d0214
m19e <m19e.dev@gmail.com>
2025-04-30 00:12:03 +0900

    💡 Remove resolved todo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,6 @@ const rampart = Rampart_One({
   weight: ['400'],
 })
 
-// TODO メタデータ設定
-// TODO 画像設定(OGP, Twitter) → 「絵文字」を重ねてロゴに
 const title = '絵文字ステッカー！ | 画像に絵文字を貼るアプリ'
 const description =
   '「絵文字ステッカー！」は画像に絵文字を貼りつけたり情報を隠すことができる画像編集アプリです。'
