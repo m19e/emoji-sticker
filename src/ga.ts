15e335093286ec1382f75994469bd13dd1f5e44e
m19e <m19e.dev@gmail.com>
2025-05-01 20:28:57 +0900

    💡 Update todo comments for custom-event-map

--- src/ga.ts
+++ src/ga.ts
@@ -8,10 +8,7 @@ export enum CustomEvent {
   Rect = 'add_rect',
 }
 
-// FIXME GA4上でemoji, rectが二重計測される
-// TODO emoji, rectイベントを削除して新しいイベントとして追加してみる
-// TODO GTM側にタグとか諸々追加してみる(二重計測されないイベントとの差分を試す)
-// TODO カスタムイベントとして追加していたため二重計測が発生していた(削除して二重でなくなった)
+// TODO 不要になったので削除
 const CustomEventMap: { [key in CustomEvent]: string } = {
   click_save_button: 'SAVE',
   click_share_button: 'SHARE',
