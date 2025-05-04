e64c8fb106bf48bc9a4d8f1616eac17f0e94bdd7
m19e <m19e.dev@gmail.com>
2025-05-05 00:01:07 +0900

    💡 Add todo about save, share event

--- src/hooks/useCanvasData.ts
+++ src/hooks/useCanvasData.ts
@@ -13,6 +13,7 @@ type Return = {
   share: () => Promise<void>
 }
 
+// TODO save,shareイベント送信時にステッカーの数も集計する?
 export const useCanvasData = (): Return => {
   const [canvasRef, setCanvasRef] = useAtom(canvasRefAtom)
   const { pixelRatio } = useCanvasSize()
@@ -57,6 +58,8 @@ export const useCanvasData = (): Return => {
     )
   }
 
+  // TODO 共有できない環境でのGA4イベント追加、送信
+  // TODO OS差分が出るためUAもイベント送信に含めたい
   const share = async () => {
     if (!isSupported()) {
       toast.error('現在の環境では共有機能をご利用いただけません')
