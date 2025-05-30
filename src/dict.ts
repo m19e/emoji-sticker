b914f1350eb7cee3554c35fafa304afa008dcc45
m19e <m19e.dev@gmail.com>
2025-05-30 15:41:53 +0900

    ✨ Add global dict

---
+++ src/dict.ts
@@ -0,0 +1,26 @@
+// TODO グローバル辞書
+// TODO 共通 > キャンセル
+// TODO 削除
+// TODO 保存
+// TODO 共有
+// TODO トースト
+const Dict = {
+  cancel: '',
+  delete: {
+    sticker: '',
+    baseImage: {
+      btn: '',
+      confirm: '',
+    },
+  },
+  save: {},
+  share: {},
+  toast: {
+    deleteBaseImage: '',
+    save: '',
+    share: {
+      success: '',
+      error: '',
+    },
+  },
+} as const
