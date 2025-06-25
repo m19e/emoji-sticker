ccedebecbf05df92829d8533a17a3223216b7bb3
m19e <m19e.dev@gmail.com>
2025-06-25 20:56:14 +0900

    💡 Remove useless comment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: 'canvas' }]
     return config
