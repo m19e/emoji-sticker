e3264c0f142397346df267f453e2814e5193b53c
m19e <m19e.dev@gmail.com>
2025-07-23 17:33:51 +0900

    ✨ Dont select rect when add rect

--- src/components/button/AddRectStickerButton.tsx
+++ src/components/button/AddRectStickerButton.tsx
@@ -4,24 +4,20 @@ import { StickyNoteIcon } from 'lucide-react'
 import { v4 } from 'uuid'
 
 import { GA4Event, sendEvent } from '@/ga'
-import {
-  isBaseImgLoadedAtom,
-  rectanglesAtom,
-  selectedStickerIdAtom,
-} from '@/store/atoms'
+import { isBaseImgLoadedAtom, rectanglesAtom } from '@/store/atoms'
 
 import { Button } from '@/components/ui/button'
 
+// TODO ID atom依存箇所を削除
 export const AddRectStickerButton = () => {
   const isLoaded = useAtomValue(isBaseImgLoadedAtom)
   const setRectangles = useSetAtom(rectanglesAtom)
-  const setSelectedStickerId = useSetAtom(selectedStickerIdAtom)
 
   const handleClick = () => {
     sendEvent(GA4Event.Rect)
 
+    // TODO 矩形追加時に選択しなければIdAtomを削除できるかも
     const id = v4()
-    setSelectedStickerId(id)
     setRectangles((prev) => [...prev, { id }])
   }
 
