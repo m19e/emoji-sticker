caf7aa8ae047fb990da8458d7db86fa488e22baa
m19e <m19e.dev@gmail.com>
2025-08-23 21:31:51 +0900

    ✨ Move sticker from canvas touch-move

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,14 @@
 'use client'
+import { useAtomValue, useSetAtom } from 'jotai'
+import type Konva from 'konva'
+import { useRef, useState } from 'react'
 import { Stage } from 'react-konva'
+
+import {
+  emojiDatasAtom,
+  rectanglesAtom,
+  selectedStickerAtom,
+} from '@/store/atoms'
 
 import { useBaseImageSize } from '@/hooks/useBaseImageSize'
 import { useCanvasData } from '@/hooks/useCanvasData'
@@ -8,12 +17,85 @@ import { useCanvasSize } from '@/hooks/useCanvasSize'
 import { BaseImageLayer } from '@/components/layer/BaseImage'
 import { StickerLayer } from '@/components/layer/Sticker'
 
+// TODO 一旦リリース onTouchでステッカー移動
 export const Canvas = () => {
-  const { ref } = useCanvasData()
+  const { ref, stage } = useCanvasData()
   const {
     canvas: { width, height },
   } = useCanvasSize()
   const { imgSize } = useBaseImageSize()
+  const selectedData = useAtomValue(selectedStickerAtom)
+  const setEmojis = useSetAtom(emojiDatasAtom)
+  const setRects = useSetAtom(rectanglesAtom)
+
+  const lastPointerPos = useRef<Konva.Vector2d>(null)
+  const [dragging, setDragging] = useState(false)
+
+  const initialPosition = {
+    x: imgSize.width / 2,
+    y: imgSize.height / 2,
+  }
+
+  const handleTouchStart = () => {
+    if (!selectedData) return
+    setDragging(true)
+
+    const pointer = stage?.getPointerPosition()
+    if (!pointer) return
+    lastPointerPos.current = pointer
+  }
+
+  const handleTouchMove = () => {
+    if (!dragging || !selectedData) return
+
+    const pointer = stage?.getPointerPosition()
+    if (!pointer || !lastPointerPos.current) return
+
+    const dx = pointer.x - lastPointerPos.current.x
+    const dy = pointer.y - lastPointerPos.current.y
+
+    lastPointerPos.current = pointer
+
+    if (selectedData.type === 'emoji') {
+      setEmojis((emojis) =>
+        emojis.map((e) => {
+          if (selectedData.id === e.id) {
+            const p = e.position ?? initialPosition
+            return {
+              ...e,
+              position: {
+                x: p.x + dx,
+                y: p.y + dy,
+              },
+            }
+          }
+
+          return e
+        }),
+      )
+    } else {
+      setRects((rects) =>
+        rects.map((r) => {
+          if (selectedData.id === r.id) {
+            const p = r.position ?? initialPosition
+            return {
+              ...r,
+              position: {
+                x: p.x + dx,
+                y: p.y + dy,
+              },
+            }
+          }
+
+          return r
+        }),
+      )
+    }
+  }
+
+  const handleTouchEnd = () => {
+    setDragging(false)
+  }
 
   return (
     <Stage
@@ -23,6 +105,9 @@ export const Canvas = () => {
       scaleX={width / imgSize.width}
       scaleY={height / imgSize.height}
       onContextMenu={(ke) => ke.evt.preventDefault()}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
     >
       <BaseImageLayer />
       <StickerLayer img={imgSize} />
