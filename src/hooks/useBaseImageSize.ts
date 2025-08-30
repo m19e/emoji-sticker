e9ed238efc914998862a89593b88a6d37aa81e1d
m19e <m19e.dev@gmail.com>
2025-08-31 00:48:21 +0900

    💡 Remove done

--- src/hooks/useBaseImageSize.ts
+++ src/hooks/useBaseImageSize.ts
@@ -4,8 +4,6 @@ import { useEffect } from 'react'
 import { useImageSize } from '@/hooks/useImageSize'
 import { baseImgSizeAtom, baseImgUrlAtom } from '@/store/atoms'
 
-// TODO imgSizeをjotaiで管理して画像読み込み処理を一度だけに抑える
-// TODO このhooksではimgSizeを返さず、atomから直接データ取得する
 export const useBaseImageSize = () => {
   const url = useAtomValue(baseImgUrlAtom)
   const setSize = useSetAtom(baseImgSizeAtom)
