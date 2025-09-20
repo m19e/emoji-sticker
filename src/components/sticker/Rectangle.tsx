50a0978b3d0b069bcf050f9f421a0d0c42183b59
m19e <m19e.dev@gmail.com>
2025-09-20 20:22:35 +0900

    💡 Remove done

--- src/components/sticker/Rectangle.tsx
+++ src/components/sticker/Rectangle.tsx
@@ -16,7 +16,6 @@ type Props = {
   h: number
 } & StickerProps
 
-// TODO 選択処理共通化
 // TODO 回転スナップ追加
 export const Rectangle = ({ id: rectId, w, h, position, isDesktop }: Props) => {
   const shapeRef = useRef<Konva.Rect>(null)
