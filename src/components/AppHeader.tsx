105a03239547b8240581b633358aafe5861bf563
m19e <m19e.dev@gmail.com>
2025-09-27 22:46:56 +0900

    💡 Remove done

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,7 +2,6 @@ import { AboutDialog } from '@/components/AboutDialog'
 import { DeleteBaseImageButton } from '@/components/button/DeleteBaseImageButton'
 import { ShareButton } from '@/components/button/ShareButton'
 
-// TODO atomを剥がしてServerComponentにしたい
 export const Header = () => {
   return (
     <div className="flex justify-between">
